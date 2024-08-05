adb4444638afde0c0d745643e00ef249a6b195a8
hyejin <dbsgpwls0316@gmail.com>
2024-08-05 17:58:38 +0900

    8_5

--- gold/BruteForce/1111.py
+++ gold/BruteForce/1111.py
@@ -2,7 +2,35 @@
 # 1111번: IQ Test
 # gold 3
 
-n = input()
+N = int(input())
 lst = list(map(int, input().split()))
+a = b = 0
 
-print(lst)
+if (N == 2 and lst[0] != lst[1]) or N <= 1:     # 다음 수가 여러 개
+    print('A')
+    exit()
+
+# 앞 수 * a + b
+# 4a + b = 13   -> a a a a b 13
+# 13a + b = 40  -> a a a a a a a a a a a a a b 40 
+# 9a = 27
+# a = 3, b = 1
+
+# a = (40-13)/(13-4)
+# b = 13 - 4*a
+
+if N == 2:
+    a = 1
+    b = 0
+else:
+    for i in range(N-2):
+        a = (lst[i+2]-lst[i+1])/(lst[i+1]-lst[i])
+        b = lst[i+1] - lst[i] * a
+        # a, b 값이 바뀌거나 a가 정수가 아니면
+
+print(a, b)
+
+if a == int(a):
+    print(lst[N-1] * a + b)
+else:       # 다음 수를 구할 수 없음
+    print('B')
--- test/test.py
+++ test/test.py
@@ -1,16 +1,10 @@
-def binarySearch(a, N, key):
-    start = 0
-    end = N-1
-    while start <= end:
-        middle = (start + end)//2
-        print(start, middle, end)
-        if a[middle] == key:    # 검색 성공
-            return True
-        elif a[middle] > key:
-            end = middle
-        else:
-            start = middle
-    print(start, middle, end)
-    return False    # 검색 실패
+a = 123.0
+b = 'adf'
 
-binarySearch([0,1,2,3,4,5,6,7,8,9,10], 11, 10)
+if a == int(a):
+    print(a)
+
+print(type(a/3443))
+print(type(a))
+print(type(b))
+print(type(int))
