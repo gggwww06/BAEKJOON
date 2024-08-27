455c2f5500bc7eed57bb2cd1dbce341831b996f9
gggwww06 <159458113+gggwww06@users.noreply.github.com>
2024-08-27 19:24:09 +0900

    Update 1389_케빈_베이컨의_6단계_법칙.py

--- silver/1389_케빈_베이컨의_6단계_법칙.py
+++ silver/1389_케빈_베이컨의_6단계_법칙.py
@@ -1,18 +1,19 @@
 # 1389 케빈 베이컨의 6단계 법칙
 # https://www.acmicpc.net/problem/1389
-#
+# 31120KB 36ms 576B
 
 def bfs(st):
     q = [st]
-    visited = [0]*(M+1)
+    visited = [-1]*(N+1)
+    visited[st] = 0
     while q:
         w = q.pop(0)
         for i in arr[w]:
-            if visited[i] == 0:
+            if visited[i] == -1:
                 q.append(i)
                 visited[i] = visited[w]+1
         if not q:
-            return max(visited)
+            return sum(visited)
 
 N, M = map(int, input().split())
 arr = [[] for _ in range(N+1)]
@@ -20,8 +21,8 @@ for _ in range(M):
     a, b = map(int, input().split())
     arr[a].append(b)
     arr[b].append(a)
-res = [0, N]
-for i in range(N):
+res = [0, N*N]
+for i in range(1, N+1):
     tmp = bfs(i)
     if res[1] > tmp:
         res[0] = i
